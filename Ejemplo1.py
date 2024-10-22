82860eb0415b017e9a92153e88f547d2807c012b
alvgarcia <agdrez48@gmail.com>
2024-10-22 10:08:12 +0200

    dudi

--- Ejemplo1.py
+++ Ejemplo1.py
@@ -4,4 +4,4 @@ num1=int(input("ingrese primer valor:"))
 num2=int(input("ingrese segundo valor:"))
 suma=num1+num2
 producto=num2*num1
-print("La suma de los valores es",suma,"El producto de los dos valores es",producto)
+print("La suma de los valores es",suma,"\nEl producto de los dos valores es",producto)
